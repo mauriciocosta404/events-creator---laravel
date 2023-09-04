196eaee2cd6242fd74c352f96877e20a0ffefa0f
mauricio <mauricio@webtagger.com.br>
2023-09-04 15:18:45 +0100

    first commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>@yield('title')</title>
+        <!-- Fonts -->
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+
+        <!-- css bootstrap -->
+
+        <!-- css app -->
+
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+        @yield('content')
+        <footer>
+            my events &copy; 2023 
+        </footer>
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+@section('title', 'My Events')
+
+@section('content')
+<h1>welcome</h1>
+@endsection
